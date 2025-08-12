a68d69da7dcb95922ac6e7075fb3a4c54b5dbd03
Colby Corcoran <colbyacorcoran@gmail.com>
2025-08-12 08:49:41 -0500

    Initial Commit

---
+++ E5 Trivia/E5_TriviaApp.swift
@@ -0,0 +1,32 @@
+//
+//  E5_TriviaApp.swift
+//  E5 Trivia
+//
+//  Created by Colby Corcoran on 8/12/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct E5_TriviaApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ E5 TriviaTests/E5_TriviaTests.swift
@@ -0,0 +1,17 @@
+//
+//  E5_TriviaTests.swift
+//  E5 TriviaTests
+//
+//  Created by Colby Corcoran on 8/12/25.
+//
+
+import Testing
+@testable import E5_Trivia
+
+struct E5_TriviaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
